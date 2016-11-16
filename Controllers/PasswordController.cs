b91285d526575340cea8d75e18a8ab36d38ecbed
Antonia Elek <antoniaelek@hotmail.com>
2016-11-16 22:54:03 +0100

    Saved passwords encryption & decryption.

--- Controllers/PasswordController.cs
+++ Controllers/PasswordController.cs
@@ -1,13 +1,16 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using RandomPasswordGenerator.Models;
 
 namespace RandomPasswordGenerator 
@@ -23,11 +26,18 @@ namespace RandomPasswordGenerator
         private string _digit = "1234567890";
         private string _nonAlphaNum = "!@#$%^&*()_-+=[{]};:<>|./?";
 
+        private IConfigurationRoot _configuration;
+
         public PasswordController(ApplicationDbContext context,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager, IHostingEnvironment env)
         {
             _context = context;
             _userManager = userManager;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
         }
 
         // POST: api/Password
@@ -37,9 +47,11 @@ namespace RandomPasswordGenerator
         {
             if (ModelState.IsValid)
             {
+                var plain = new string(GetRandomPass(viewModel));
+                var encrypted = Encrypt(plain);
                 var password = new Password()
                 {
-                    PasswordText = new string(GetRandomPass(viewModel)),
+                    PasswordText = encrypted,
                     DateCreated = DateTime.Now,
                     Hint = viewModel.Hint
                 };
@@ -49,7 +61,7 @@ namespace RandomPasswordGenerator
                     return new JsonResult(new
                     {
                         Success = true,
-                        Password = password.PasswordText,
+                        Password = plain,
                         DateCreated = password.DateCreated
                     });
 
@@ -59,7 +71,7 @@ namespace RandomPasswordGenerator
                 {
                     Success = true,
                     Id = password.Id,
-                    Password = password.PasswordText,
+                    Password = plain,
                     Hint = password.Hint,
                     UserId = password.UserId,
                     DateCreated = password.DateCreated
@@ -96,7 +108,7 @@ namespace RandomPasswordGenerator
             {
                 Success = true,
                 Id = pass.Id,
-                Password = pass.PasswordText,
+                Password = Decrypt(pass.PasswordText),
                 Hint = pass.Hint,
                 UserId = pass.UserId,
                 DateCreated = pass.DateCreated
@@ -159,6 +171,48 @@ namespace RandomPasswordGenerator
                 gen.GetBytes(data);
                 return BitConverter.ToUInt16(data, 0) % max;
             }
+        }
+        
+        private string Encrypt(string clearText)
+        {
+            var EncryptionKey = _configuration.GetConnectionString("Enc");
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+        private string Decrypt(string cipherText)
+        {
+            var EncryptionKey = _configuration.GetConnectionString("Enc");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
         }
     }
 }
