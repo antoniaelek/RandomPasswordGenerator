a59cc00942be451b0ed06a15a0ae4823fa67ae4e
Antonia Elek <antoniaelek@hotmail.com>
2016-11-22 01:42:53 +0100

    Project structure and dependencies for testing.

---
+++ test/UnitTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace RandomPasswordGenerator.Test
+{
+    public class UnitTests
+    {
+         public UnitTests()
+         {
+         }
+
+        [Fact]
+        public void TestThing() {
+            Assert.Equal(42, 42);
+        }
+    }
+}
